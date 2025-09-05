dde7cf740ffc7902189d703055831b4facbabf02
Octavian T. <octavian@tabuci.org>
2025-09-05 13:02:47 +0300

    ParseBool

--- conv/bool.go
+++ conv/bool.go
@@ -3,37 +3,37 @@ package conv
 import "strings"
 
 func ParseBool(val interface{}) bool {
-	switch val.(type) {
-	case string:
-		sVal := strings.ToLower(val.(string))
-		switch sVal {
-		case "yes":
-			return true
-		case "no":
-			return false
-		case "true":
-			return true
-		case "false":
-			return false
-		case "0":
-			return false
-		case "1":
-			return true
-		default:
-			return false
-		}
-	default:
-		switch val {
-		case true:
-			return true
-		case false:
-			return false
-		case 1:
-			return true
-		case 0:
-			return false
-		default:
-			return false
-		}
-	}
+    switch v := val.(type) {
+    case string:
+        sVal := strings.ToLower(v)
+        switch sVal {
+        case "yes":
+            return true
+        case "no":
+            return false
+        case "true":
+            return true
+        case "false":
+            return false
+        case "0":
+            return false
+        case "1":
+            return true
+        default:
+            return false
+        }
+    default:
+        switch val {
+        case true:
+            return true
+        case false:
+            return false
+        case 1:
+            return true
+        case 0:
+            return false
+        default:
+            return false
+        }
+    }
 }
